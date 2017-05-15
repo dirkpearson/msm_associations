f67ca2be89d79e0874fe4e2af205fc56f0ef30e9
rails -v <dirk.pearson@yhlsoft.com>
2017-05-15 12:27:10 -0500

    96.8%

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,8 @@ class Actor < ApplicationRecord
  validates :name, :presence => true, :uniqueness => { :scope => :dob}
 
  # - dob: no rules
+ has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+has_many(:movies, :class_name => "Movie", :through => :characters, :foreign_key => "actor_id")
  # - bio: no rules
  # - image_url: no rules
 end
